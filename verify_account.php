c1e27261cf6e670330abde813c80347297b2338e
ryan-greaves <80297990+ryan-greaves@users.noreply.github.com>
2024-06-22 17:48:16 -0700

    Create verify_account.php

---
+++ verify_account.php
@@ -0,0 +1,98 @@
+<?php
+	session_start();
+	if(!isset($_SESSION["email"]))
+	{
+		header("Location: register.php"); // Redirect back to register.php if the user tried to go directly to this page.
+		exit;
+	}
+	else
+	{
+		$email = $_SESSION["email"];
+	}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Verification</title>
+    <?php include 'bootstrap.html'?>
+</head>
+
+<body>
+	<p>The code to complete verification has been sent your email at <?php echo $email; ?>.</p>
+	<p>The code will expire in 5 minutes.</p>
+	<form name="account_verification" method="post" action="">
+		<div class="col-sm-3 mb-2">
+			<label for="username" class="form-label">Enter Verification Code:</label>
+			<input type="text" name="code" class="form-control" id="code" required>
+		</div>
+		<button type="submit" name="submit" class="btn btn-primary">Submit</button>
+	</form>
+</body>
+
+<?php
+	$sql_servername = "localhost";
+	$sql_username = "root";
+	$sql_password = "";
+	$sql_dbname = "nutritional_tracker";
+
+	$conn = new mysqli($sql_servername, $sql_username, $sql_password, $sql_dbname);
+
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}	
+
+	$verified = false;
+	$code_expired = false;
+	if(isset($_POST["submit"]))
+	{
+		$sql = "SELECT * FROM unverified_users WHERE email = '$email' LIMIT 1";
+		$results = $conn->query($sql);
+		if($results->num_rows > 0) // If there is a matching entry in unverified_users
+			$result = $results->fetch_object();
+		else
+			$code_expired = true;
+			
+		if(isset($result->expires) && time() < $result->expires) // If the current time is not past the code's expiration time.
+			$correct_code = $result->code;
+		else
+			$code_expired = true;
+		
+		if(!$code_expired && $_POST["code"] == $correct_code) // If the correct code is entered by the user.
+		{
+			$username = $result->username;
+			$password = $result->password;
+
+			$sql = "INSERT INTO users (email, username, password) VALUES ('$email', '$username', '$password')";
+			$conn->query($sql);
+			$verified = true; // Account verified
+			
+			// Proceed to login page
+			// Perhaps include an intermediary page, stating that the email is now verified.
+			echo "<p>Your email has been succesfully verified!</p>";
+			header('Refresh: 2; url=index.php'); // Placeholder
+		}
+		else if ($code_expired)
+		{
+			echo "<p>The code has expired.</p>";
+			header('Refresh: 2; url=register.php'); // Redirect back to the registration page after a few seconds.
+		}
+		else if ($_POST["code"] != $correct_code)
+		{
+			echo "<p>Incorrect code.</p>"; //
+		}
+	}
+
+	if($verified || $code_expired)
+	{
+		$sql = "DELETE FROM unverified_users WHERE email = '$email'";
+		$conn->query($sql);
+		session_destroy();
+		session_unset();
+	}
+?>
+
+
