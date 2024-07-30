e88cb2bec2798636a530ec06bf2f1b571a61481d
ryan-greaves <80297990+ryan-greaves@users.noreply.github.com>
2024-07-29 18:30:19 -0700

    Removed <title> from nav.php
    This fix stops nav.php from overriding titles of other pages that include it.

--- nav.php
+++ nav.php
@@ -15,7 +15,6 @@ if (isset($_POST['delete_session']))
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meal Tracker</title>
     <base href="/INFO_4290_Project/">
 </head>
 
